af905209c33f5537820a813fc797b0c27085d2b8
Dan Ramos <dramos287@gmail.com>
2024-10-15 08:59:41 -0400

    styles and mui

--- app.config.ts
+++ app.config.ts
@@ -1,4 +1,12 @@
-// app.config.ts
 import { defineConfig } from "@tanstack/start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
+  },
+});
--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -3,6 +3,8 @@ import { createRootRoute } from "@tanstack/react-router";
 import { Outlet, ScrollRestoration } from "@tanstack/react-router";
 import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
 import * as React from "react";
+import appCss from "~/styles/app.css?url";
+import "@fontsource-variable/lexend-deca";
 
 export const Route = createRootRoute({
   meta: () => [
@@ -16,6 +18,28 @@ export const Route = createRootRoute({
     {
       title: "TanStack Start Starter",
     },
+  ],
+  links: () => [
+    { rel: "stylesheet", href: appCss },
+    // {
+    //   rel: 'apple-touch-icon',
+    //   sizes: '180x180',
+    //   href: '/apple-touch-icon.png',
+    // },
+    // {
+    //   rel: 'icon',
+    //   type: 'image/png',
+    //   sizes: '32x32',
+    //   href: '/favicon-32x32.png',
+    // },
+    // {
+    //   rel: 'icon',
+    //   type: 'image/png',
+    //   sizes: '16x16',
+    //   href: '/favicon-16x16.png',
+    // },
+    // { rel: 'manifest', href: '/site.webmanifest', color: '#fffff' },
+    // { rel: 'icon', href: '/favicon.ico' },
   ],
   component: RootComponent,
 });
