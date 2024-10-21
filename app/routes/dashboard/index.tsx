eb0bdc0a7b520616309a8934a6baf51b022496f1
Dan Ramos <dramos287@gmail.com>
2024-10-21 09:54:37 -0400

    add iframe

--- app/routes/dashboard/index.tsx
+++ app/routes/dashboard/index.tsx
@@ -2,5 +2,16 @@ import { createFileRoute } from "@tanstack/react-router";
 import MainGrid from "~/components/MainGrid";
 
 export const Route = createFileRoute("/dashboard/")({
-  component: () => <MainGrid />,
+  component: () => (
+    <iframe
+      title="TLC Dashboard v0.4"
+      style={{
+        width: "100%",
+        height: "90vh",
+      }}
+      src="https://app.powerbi.com/view?r=eyJrIjoiNTdiNTExM2EtMWNjYS00ZWQ1LWIwZTEtYjVlNGE1NDg4ZDQ0IiwidCI6IjY1ZTQ5N2M5LTMyNTQtNDVmZi05YTFlLWQyMzQ2MjM0MWVkMCIsImMiOjN9"
+      frameborder="0"
+      allowfullscreen="true"
+    ></iframe>
+  ),
 });
