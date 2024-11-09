141a14c94677a96f1d5cb6f3ef0478b7f021dc54
Dan Ramos <dramos287@gmail.com>
2024-11-09 15:26:54 -0500

    fix build config

--- app.config.ts
+++ app.config.ts
@@ -3,17 +3,31 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   vite: {
-    // ssr: {
-    //   noExternal: [
-    //     "@mui/system",
-    //     "@mui/icons-material",
-    //     "@mui/lab",
-    //     "@mui/material",
-    //     "@mui/styles",
-    //     "@mui/x-date-pickers",
-    //     "@mui/util",
-    //   ],
-    // },
+    build: {
+      rollupOptions: {
+        external: ["@mui/utils"],
+        output: {
+          manualChunks: {
+            mui: ["@mui/material", "@mui/icons-material"],
+          },
+        },
+      },
+    },
+    optimizeDeps: {
+      include: ["@mui/material", "@mui/icons-material"],
+      exclude: ["@mui/utils"],
+    },
+    ssr: {
+      noExternal: [
+        "@mui/system",
+        "@mui/icons-material",
+        "@mui/lab",
+        "@mui/material",
+        "@mui/styles",
+        "@mui/x-date-pickers",
+        "@mui/util",
+      ],
+    },
     plugins: [
       tsConfigPaths({
         projects: ["./tsconfig.json"],
