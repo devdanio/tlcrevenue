1eaec034583519fba5433ecdd74159a5db91c3bd
Dan Ramos <dramos287@gmail.com>
2024-11-09 15:05:10 -0500

    updated server deps

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,16 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   vite: {
+    ssr: {
+      noExternal: [
+        "@mui/system",
+        "@mui/icons-material",
+        "@mui/lab",
+        "@mui/material",
+        "@mui/styles",
+        "@mui/x-date-pickers",
+      ],
+    },
     plugins: [
       tsConfigPaths({
         projects: ["./tsconfig.json"],
