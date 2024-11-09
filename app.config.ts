b43d283bb726315eaba709e7c93d69e8e90a64e8
Dan Ramos <dramos287@gmail.com>
2024-11-09 15:23:08 -0500

    fixed deps

--- app.config.ts
+++ app.config.ts
@@ -3,17 +3,17 @@ import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   vite: {
-    ssr: {
-      noExternal: [
-        "@mui/system",
-        "@mui/icons-material",
-        "@mui/lab",
-        "@mui/material",
-        "@mui/styles",
-        "@mui/x-date-pickers",
-        "@mui/util",
-      ],
-    },
+    // ssr: {
+    //   noExternal: [
+    //     "@mui/system",
+    //     "@mui/icons-material",
+    //     "@mui/lab",
+    //     "@mui/material",
+    //     "@mui/styles",
+    //     "@mui/x-date-pickers",
+    //     "@mui/util",
+    //   ],
+    // },
     plugins: [
       tsConfigPaths({
         projects: ["./tsconfig.json"],
