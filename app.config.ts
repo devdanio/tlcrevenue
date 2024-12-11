cdcbe626b0d71903b1c85cd116a1256ad00653cb
Dan Ramos <dramos287@gmail.com>
2024-12-11 13:38:08 -0500

    setting the app config to use vercel

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from "@tanstack/start/config";
 import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    preset: "vercel",
+  },
+
   vite: {
     plugins: [
       tsConfigPaths({
